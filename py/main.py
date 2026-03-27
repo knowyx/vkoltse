9d637271befb378abe051c83e27da1ce02a956b9
knowyx <knowyx@gmail.com>
2026-03-27 19:10:32 +0300

    Starting to develop footer and header, using bootstrap now

---
+++ py/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+WEBDIRPATH = 'html/'
+
+app = Flask(__name__, template_folder='../html', static_folder="../static")
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", pagename="Главная", usr="knowyx_")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
