ecebc9d8e61ff5a7d939a38e9e5911b0b70372bc
perdun <andrejnnefedov72@gmail.com>
2026-04-01 17:39:35 +0300

    alpha 0.0.1 added db and api for them

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
 from requests import get, post, delete, put
-
-post(
-"http://127.0.0.1:8080/api/users",
-    json={
-        "permissions": "Лох",
-        "email": "perdun322@gmail.com",
-        "login": "2026-04-01",
-        "password": "322"
-    }
-)
