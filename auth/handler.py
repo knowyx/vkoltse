271ff2a81da41ba56fa3b60f4f4670deb8060a9c
knowyx <knowyx@gmail.com>
2026-04-06 16:55:53 +0300

    added cheking for outdated cookie and incorrent user agent

--- auth/handler.py
+++ auth/handler.py
@@ -32,7 +32,7 @@ def username_exist(username, db_session, User):
         return False
 
 
-def remove_auth_sessions(email, db_session, Sessions, User):
+def remove_auth_sessions_email(email, db_session, Sessions, User):
     with db_session.create_session() as db_session:
         user = db_session.query(User).filter(User.email == email).first()
         db_session.query(Sessions).filter(Sessions.user_id == user.id).delete()
@@ -40,7 +40,7 @@ def remove_auth_sessions(email, db_session, Sessions, User):
 
 
 def create_auth_session(email, db_session, Sessions, User):
-    remove_auth_sessions(email, db_session, Sessions, User)
+    remove_auth_sessions_email(email, db_session, Sessions, User)
     with db_session.create_session() as db_session:
         user = db_session.query(User).filter(User.email == email).first()
         session = Sessions()
@@ -68,6 +68,19 @@ def login_user(email, password, db_session, User):
         return False
     
 
+def check_data_and_ua(session_data, db_session, Sessions, User):
+    if datetime.now() < session_data.auth_date + timedelta(days=10):
+        if request.headers.get('User-Agent') == session_data.user_agent:
+            return True
+        else:
+            return False
+    else:
+        with db_session.create_session() as db_session:
+            db_session.query(Sessions).filter(Sessions.session_key == session_data.session_key).delete()
+            db_session.commit()
+        return False
+        
+
 def auth_user_view(db_session, User, Sessions):
     cookie_data = request.cookies.get("session_key (DO NOT SHARE WITH ANYONE!)")
     base_button = '''
@@ -75,28 +88,41 @@ def auth_user_view(db_session, User, Sessions):
                         <a href="/auth"><button type="button" class="btn btn-dark">Войти</button></a>
                     </div>
                     '''
-    if cookie_data != None:
-            with db_session.create_session() as db_session:
-                session = db_session.query(Sessions).filter(Sessions.session_key == cookie_data).first()
-                # проверка даты и юзерагента
-                user = db_session.query(User).filter(User.id == session.user_id).first()
-                if user.permissions == 'Admin':
-                    admin_link = '''<li><a class="dropdown-item" href="/admin-cabinet">Кабинет Администратора</a></li>'''
-                else:
-                    admin_link = ''
-                return f'''
-                        <div class="d-flex ms-auto">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-                                    {user.login}
-                                </button>
-                                <ul class="dropdown-menu dropdown-menu-lg-end">
-                                    <li><a class="dropdown-item" href="/dashboard">Личный кабинет</a></li>
-                                    {admin_link}
-                                    <li><a class="dropdown-item" href="/auth/logout">Выйти</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        '''
-    else:
+    
+    if cookie_data == None:
         return base_button
+    
+    with db_session.create_session() as db_session:
+        try:
+            session_data = db_session.query(Sessions).filter(Sessions.session_key == cookie_data).first()
+            user = db_session.query(User).filter(User.id == session_data.user_id).first()
+        except AttributeError:
+            return base_button
+
+        if datetime.now() > session_data.auth_date + timedelta(days=10):
+            db_session.query(Sessions).filter(Sessions.session_key == cookie_data).delete()
+            db_session.commit()
+            return base_button
+    
+    if request.headers.get('User-Agent') != session_data.user_agent:
+        return base_button
+    
+    if user.permissions == 'Admin':
+        admin_link = '''<li><a class="dropdown-item" href="/admin-cabinet">Кабинет Администратора</a></li>'''
+    else:
+        admin_link = ''
+    return f'''
+        <div class="d-flex ms-auto">
+            <div class="btn-group">
+                <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                    {user.login}
+                </button>
+                <ul class="dropdown-menu dropdown-menu-lg-end">
+                    <li><a class="dropdown-item" href="/dashboard">Личный кабинет</a></li>
+                    {admin_link}
+                    <li><a class="dropdown-item" href="/auth/logout">Выйти</a></li>
+                </ul>
+            </div>
+        </div>
+        '''
+        
