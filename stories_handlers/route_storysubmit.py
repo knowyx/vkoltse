4568cbb62b402525f8d721cfc1ce20511476fb86
perdun <andrejnnefedov72@gmail.com>
2026-04-11 18:52:11 +0300

    start

--- stories_handlers/route_storysubmit.py
+++ stories_handlers/route_storysubmit.py
@@ -1,1 +1,23 @@
+from data import db_session
+from data.stories import Stories
 from .__init__ import story_blueprint
+from flask import render_template, redirect, url_for
+from .forms import StorySubmitForm
+
+
+@story_blueprint.route('/story_submit')
+def story_submit():
+    form = StorySubmitForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        story = Stories(
+            title=form.title.data,
+            content=form.content.data,
+            date=form.date.data
+        )
+        session.add(story)
+        session.commit()
+        
+        return redirect(url_for('stories_list'))
+
+    return render_template('story_submit.html', form=form)
